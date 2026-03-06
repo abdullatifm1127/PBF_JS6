7f445166fe4d6d0deb3393829f63eebcd63ea3e6
latif <mabdullatifm1127@gmail.com>
2026-03-07 01:06:58 +0700

    Langkah 6

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,6 +4,7 @@ import styles from '@/styles/404.module.scss'
 const Custom404 = () => {
     return (
         <div className={styles.error}>
+            <img src="/error-404.png" alt="404" className={styles.error__image} />
             <h1>404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf, Halaman yang anda cari tidak ada</p>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
-import styles from "@/styles/404.module.scss";
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/navbar'
 
-const Custom404 = () => {
-    return (
-        <div className={styles.error}>
-          <img src="/page-not-found.png" alt="404" className={styles.error__image} />
-            <h1>404 - Halaman Tidak Ditemukan</h1>
-            <p>Maaf, Halaman yang anda cari tidak ada</p>
-        </div>
-    );
-};
+const inter = Inter({ subsets: ['latin'] })
 
-export default Custom404;
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Praktikum Next.js Pages Router</title>
+      </head>
+      
+      <Navbar />
+
+      <h1>Praktikum Next.js Pages Router abdul</h1>
+      <p>Mahasiswa D4 Teknik InfromatikaPengembangan Web</p>
+    </div>
+  )
+}
+
+    
